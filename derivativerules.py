1ffe3464083be62a4454ed28a31501cce98152f0
Dixon <59415488+radixon@users.noreply.github.com>
2025-03-08 16:49:17 -0500

    Update derivativerules.py

--- derivativerules.py
+++ derivativerules.py
@@ -10,8 +10,11 @@ print('----------Constant Function Rule----------')
             f'(x) = 0
 
     Parameters:
-        x:  required to make sure the constant function is part of the computation graph
+        x:  The function argument
         c:  A constant value that is defaulted to zero
+
+    Note:
+        x is multiplied by 0.  Torch find a derivative without a function argument
 """
 
 x = torch.tensor(1.0, requires_grad=True)
@@ -39,7 +42,7 @@ print('----------Identity Function Rule----------')
             f'(x) = 1
         
         Parameters:
-            x:  The value for which f'(x) will be evaluated 
+            x:  The function argument 
             c:  A constant value that is defaulted to zero
 """
 
@@ -69,7 +72,7 @@ print('----------Constant Multiple Rule----------')
             f'(x) = c
         
         Parameters:
-            x:  The value for which f'(x) will be evaluated 
+            x:  The function argument
             c:  A constant value that is defaulted to zero
 """
 
@@ -99,7 +102,7 @@ print('----------------Power Rule----------------')
             f'(x) = n * x**(n - 1)
         
         Parameters:
-            func: function passed through
+            x: function argument
 """
 
 
@@ -126,7 +129,7 @@ print('-----------------Sum Rule-----------------')
             (f + g)'(x) = f'(x) + g'(x)
         
         Parameters:
-            func: function passed through
+            x: function argument
 """
 
 
@@ -147,28 +150,24 @@ print('------------------------------------------')
 print('---------(f - g)(x) = f(x) - g(x)---------')
 print('------(f - g)\'(x) = f\'(x) - g\'(x)------')
 print('--------------Difference Rule--------------')
-def difference_rule(func):
-    """
+"""
     Difference Rule: 
         Given (f - g)(x), the derivative of (f - g)(x) is given by
             (f - g)'(x) = f'(x) - g'(x)
         
         Parameters:
-            func: function passed through
-    """
-    return func
+            x: function argument
+"""
 
 
 x = torch.tensor(2.0, requires_grad=True)
-func = 4*x**6 - 3*x**5 - 10*x**2 + 5*x + 16
-y = difference_rule(func)
+y = 4*x**6 - 3*x**5 - 10*x**2 + 5*x + 16
 y.backward()
 print(f'f(x) = 4x**6 - 3x**5 - 10x**2 + 5x + 16')
 print(f'f\'(2.0) = {x.grad} \n')
 
 x = torch.tensor(3.0, requires_grad=True)
-func = x**3 + 6*x**2 + 8*x + 1
-y = difference_rule(func)
+y = x**3 + 6*x**2 + 8*x + 1
 y.backward()
 print(f'f(x) = x**3 - 6x**2 - 8x - 1')
 print(f'f\'(3.0) = {x.grad} \n')
@@ -178,28 +177,24 @@ print('------------------------------------------')
 print('---------(f * g)(x) = f(x) * g(x)---------')
 print('(f * g)\'(x) = f(x) * g\'(x) + f\'(x) * g(x)')
 print('---------------Product Rule---------------')
-def product_rule(func):
-    """
-    Difference Rule: 
-        Given (f * g)(x), the derivative of (f * g)(x) is given by
-            (f * g)'(x) = f(x) * g'(x) + f'(x) * g(x)
-        
-        Parameters:
-            func: function passed through
-    """
-    return func
+"""
+Difference Rule: 
+    Given (f * g)(x), the derivative of (f * g)(x) is given by
+        (f * g)'(x) = f(x) * g'(x) + f'(x) * g(x)
+    
+    Parameters:
+        x: function argument
+"""
 
 
 x = torch.tensor(5.0, requires_grad=True)
-func = (x**4 - 1)*(x**2 + 1)
-y = product_rule(func)
+y = (x**4 - 1)*(x**2 + 1)
 y.backward()
 print(f'f(x) = (x**4 - 1)(x**2 + 1)')
 print(f'f\'(5.0) = {x.grad} \n')
 
 x = torch.tensor(10.0, requires_grad=True)
-func = (x**2 + 17)*(x**3 - 3*x + 1)
-y = product_rule(func)
+y = (x**2 + 17)*(x**3 - 3*x + 1)
 y.backward()
 print(f'f(x) = (x**2 + 17)(x**3 - 3*x + 1)')
 print(f'f\'(10.0) = {x.grad} \n')
