9c0d926ef4dcc41418d4b0ef7f6a1298abe296cc
Dixon <59415488+radixon@users.noreply.github.com>
2025-03-08 16:39:44 -0500

    Add files via upload

---
+++ derivativerules.py
@@ -0,0 +1,205 @@
+import torch
+
+print('------------------------------------------')
+print('-----------------f(x) = c-----------------')
+print('----------------f\'(x) = 0----------------')
+print('----------Constant Function Rule----------')
+"""
+    Constant Function
+        Given f(x) = c, the derivative of f(x) is give by
+            f'(x) = 0
+
+    Parameters:
+        x:  required to make sure the constant function is part of the computation graph
+        c:  A constant value that is defaulted to zero
+"""
+
+x = torch.tensor(1.0, requires_grad=True)
+c = torch.tensor(3.0)
+y = x*0 + c
+y.backward()
+print(f'f(x) = 3.0')
+print(f'f\'(1.0) = {x.grad} \n')
+
+
+c = torch.pi
+y = x*0 + c
+y.backward()
+print(f'f(x) = π')
+print(f'f\'(1.0) = {x.grad} \n')
+
+
+print('------------------------------------------')
+print('-----------------f(x) = x-----------------')
+print('----------------f\'(x) = 1----------------')
+print('----------Identity Function Rule----------')
+"""
+    Identity Function: 
+        Given f(x) = x, the derivative of f(x) is given by
+            f'(x) = 1
+        
+        Parameters:
+            x:  The value for which f'(x) will be evaluated 
+            c:  A constant value that is defaulted to zero
+"""
+
+
+x = torch.tensor(1000.0, requires_grad=True)
+c = torch.tensor(0.0)
+y = x + c
+y.backward()
+print(f'f(x) = x')
+print(f'f\'(1000) = {x.grad} \n')
+
+x = torch.tensor(25.0, requires_grad=True)
+c = torch.tensor(3.0)
+y = x + c
+y.backward()
+print(f'f(x) = x + 3.0')
+print(f'f\'(25.0) = {x.grad} \n')
+
+
+print('------------------------------------------')
+print('----------------f(x) = c * x----------------')
+print('----------------f\'(x) = c----------------')
+print('----------Constant Multiple Rule----------')
+"""
+    Constant Multiple Rule: 
+        Given f(x) = c*x, the derivative of f(x) is given by
+            f'(x) = c
+        
+        Parameters:
+            x:  The value for which f'(x) will be evaluated 
+            c:  A constant value that is defaulted to zero
+"""
+
+
+x = torch.tensor(25.0, requires_grad=True)
+c = torch.tensor(3.0)
+y = c * x
+y.backward()
+print(f'f(x) = 3.0 * x')
+print(f'f\'(25.0) = {x.grad} \n')
+
+x = torch.tensor(1000.0, requires_grad=True)
+c = torch.pi
+y = c * x
+y.backward()
+print(f'f(x) = π * x')
+print(f'f\'(1000.0) = {x.grad} \n')
+
+
+print('------------------------------------------')
+print('---------------f(x) =  x**n---------------')
+print('----------f\'(x) = n * x**(n - 1)----------')
+print('----------------Power Rule----------------')
+"""
+    Power Rule: 
+        Given f(x) = x**n, the derivative of f(x) is given by
+            f'(x) = n * x**(n - 1)
+        
+        Parameters:
+            func: function passed through
+"""
+
+
+x = torch.tensor(4.0, requires_grad=True)
+y = x**3
+y.backward()
+print(f'f(x) = x**3')
+print(f'f\'(4.0) = {x.grad} \n')
+
+x = torch.tensor(7.0, requires_grad=True)
+y = 2 * x**4
+y.backward()
+print(f'f(x) = 2 * x**4')
+print(f'f\'(7.0) = {x.grad} \n')
+
+
+print('------------------------------------------')
+print('---------(f + g)(x) = f(x) + g(x)---------')
+print('------(f + g)\'(x) = f\'(x) + g\'(x)------')
+print('-----------------Sum Rule-----------------')
+"""
+    Sum Rule: 
+        Given (f + g)(x), the derivative of (f + g)(x) is given by
+            (f + g)'(x) = f'(x) + g'(x)
+        
+        Parameters:
+            func: function passed through
+"""
+
+
+x = torch.tensor(35.0, requires_grad=True)
+y = 5*x**2 + 7 * x - 6
+y.backward()
+print(f'f(x) = 5x**2 + 7x - 6')
+print(f'f\'(35.0) = {x.grad} \n')
+
+x = torch.tensor(8.0, requires_grad=True)
+y = x**3 + 6*x**2 + 8*x + 1
+y.backward()
+print(f'f(x) = x**3 + 6x**2 + 8x + 1')
+print(f'f\'(8.0) = {x.grad} \n')
+
+
+print('------------------------------------------')
+print('---------(f - g)(x) = f(x) - g(x)---------')
+print('------(f - g)\'(x) = f\'(x) - g\'(x)------')
+print('--------------Difference Rule--------------')
+def difference_rule(func):
+    """
+    Difference Rule: 
+        Given (f - g)(x), the derivative of (f - g)(x) is given by
+            (f - g)'(x) = f'(x) - g'(x)
+        
+        Parameters:
+            func: function passed through
+    """
+    return func
+
+
+x = torch.tensor(2.0, requires_grad=True)
+func = 4*x**6 - 3*x**5 - 10*x**2 + 5*x + 16
+y = difference_rule(func)
+y.backward()
+print(f'f(x) = 4x**6 - 3x**5 - 10x**2 + 5x + 16')
+print(f'f\'(2.0) = {x.grad} \n')
+
+x = torch.tensor(3.0, requires_grad=True)
+func = x**3 + 6*x**2 + 8*x + 1
+y = difference_rule(func)
+y.backward()
+print(f'f(x) = x**3 - 6x**2 - 8x - 1')
+print(f'f\'(3.0) = {x.grad} \n')
+
+
+print('------------------------------------------')
+print('---------(f * g)(x) = f(x) * g(x)---------')
+print('(f * g)\'(x) = f(x) * g\'(x) + f\'(x) * g(x)')
+print('---------------Product Rule---------------')
+def product_rule(func):
+    """
+    Difference Rule: 
+        Given (f * g)(x), the derivative of (f * g)(x) is given by
+            (f * g)'(x) = f(x) * g'(x) + f'(x) * g(x)
+        
+        Parameters:
+            func: function passed through
+    """
+    return func
+
+
+x = torch.tensor(5.0, requires_grad=True)
+func = (x**4 - 1)*(x**2 + 1)
+y = product_rule(func)
+y.backward()
+print(f'f(x) = (x**4 - 1)(x**2 + 1)')
+print(f'f\'(5.0) = {x.grad} \n')
+
+x = torch.tensor(10.0, requires_grad=True)
+func = (x**2 + 17)*(x**3 - 3*x + 1)
+y = product_rule(func)
+y.backward()
+print(f'f(x) = (x**2 + 17)(x**3 - 3*x + 1)')
+print(f'f\'(10.0) = {x.grad} \n')
